3e59f8cc20c5699b5893e3b7e5c985d487d1f9b1
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2019-07-24 01:09:58 +1000

    install and config babel

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      { targets: '> 0.25%, not dead' }
+    ],
+    '@babel/preset-react'
+  ];
+  
+  const plugins = [];
+
+  return {
+    presets,
+    plugins
+  };
+};
